bc2fb818f823413aef2d3b7c88f53ed8c9116347
dyep.frontend <rdylan3871@gmail.com>
2023-09-26 22:47:16 -0600

    inicio modificado

--- src/components/Inicio.jsx
+++ src/components/Inicio.jsx
@@ -5,13 +5,12 @@ import 'react-responsive-carousel/lib/styles/carousel.min.css';
 import '../assets/css/Inicio.css';
 
 // Importa las imágenes desde la carpeta de assets
-import imagen1 from '../assets/images/parejas.jpg';
-import imagen2 from '../assets/images/parejas4.jpg';
-import imagen3 from '../assets/images/parejas2.jpg';
-import diabetesImage from '../assets/images/Diabetes.jpg'; // Importa la imagen Diabetes.jpg
+import imagen1 from '../assets/images/azucar.jpg';
+import imagen2 from '../assets/images/diabetes.png';
+import imagen3 from '../assets/images/diabetesimg.jpg';
+import diabetesImage from '../assets/images/Diabetes.jpg';
 
 const Inicio = () => {
-  // Lista de imágenes con rutas importadas
   const images = [
     { src: imagen1, alt: 'Imagen 1' },
     { src: imagen2, alt: 'Imagen 2' },
@@ -19,56 +18,122 @@ const Inicio = () => {
   ];
 
   return (
-    <div>
+    <div className="inicio-container">
       <Navbar />
 
-      <div className="inicio-container">
-        <div className="intro-section">
-          {/* Aplica estilos al contenedor para alinear la imagen y el texto */}
-          <div className="image-text-container">
-            {/* Utiliza clases de Bootstrap para el diseño responsivo */}
-            <div className="d-flex flex-column flex-md-row align-items-md-start">
-              {/* Ajusta el estilo de la imagen para que sea más pequeña */}
-              <div className="img-container">
+      <div className="container mt-5">
+        <div className="row">
+          <div className="col-md-6">
+            <div className="card">
+             
+              <div className="card-body">
+                <h1 className="card-title">Diabetes Mellitus</h1>
                 <img
-                  src={diabetesImage}
-                  alt="Diabetes Mellitus"
-                  className="img-fluid mw-100" // Clase para hacer la imagen responsiva y ocupar todo el ancho
-                  style={{ maxWidth: '250px', maxHeight: '300px' }} // Estilo personalizado para el tamaño de la imagen
-                />
+                src={diabetesImage}
+                alt="Diabetes Mellitus"
+                className="card-img-top"
+                style={{ maxWidth: '250px', maxHeight: '300px' }}
+              />
               </div>
-              <div className="text-container">
-                <h1 className="title-box">Diabetes Mellitus</h1>
+            </div>
+          </div>
+          <div className="col-md-6">
+            <Carousel>
+              {images.map((image, index) => (
+                <div key={index} className="custom-carousel-item">
+                  <img
+                    src={image.src}
+                    alt={image.alt}
+                    className="carousel-image img-fluid"
+                  />
+                </div>
+              ))}
+            </Carousel>
+          </div>
+        </div>
+      </div>
+
+      <div className="container mt-5">
+        <div className="row">
+          <div className="col-md-12">
+            <div className="card">
+              <div className="card-body">
+                <h2 className="card-title">
+                  <span className="highlight-text">¿Qué es la Diabetes Mellitus?</span>
+                </h2>
+                <p className="card-text">
+                  La diabetes mellitus (DM) es una enfermedad metabólica crónica
+                  que está adquiriendo proporciones de epidemia en los últimos
+                  años.
+                </p>
               </div>
             </div>
           </div>
         </div>
 
-        <div className="carousel-container">
-          {/* Carrusel de imágenes con clases personalizadas */}
-          <Carousel className="custom-carousel">
-            {images.map((image, index) => (
-              <div key={index} className="custom-carousel-item">
-                {/* Ajusta el estilo de las imágenes del carrusel para que sean más pequeñas */}
-                <img
-                  src={image.src}
-                  alt={image.alt}
-                  className="carousel-image"
-                />
+        <div className="row">
+          <div className="col-md-12">
+            <div className="card">
+              <div className="card-body">
+                <h2 className="card-title">Se Caracteriza por</h2>
+                <p className="card-text">
+                  Se caracteriza por alteraciones en los niveles de azúcar en
+                  la sangre, lo que puede tener graves repercusiones en la salud
+                  si no se controla adecuadamente.
+                </p>
               </div>
-            ))}
-          </Carousel>
+            </div>
+          </div>
         </div>
 
-        {/* Área de información con clases personalizadas */}
-        <div className="info-container">
-          <div className="info-section custom-info-section">
-            <h2 className="info-title">¿Qué es la Diabetes Mellitus?</h2>
-            <p className="info-text">
-              La diabetes mellitus (DM) es una enfermedad metabólica crónica que está adquiriendo proporciones de epidemia en los últimos años.
-            </p>
+        <div className="row">
+          <div className="col-md-12">
+            <div className="card">
+              <div className="card-body">
+                <h2 className="card-title">Tipos de Diabetes:</h2>
+                <ul className="card-text">
+                  <li>Diabetes Tipo 1</li>
+                  <li>Diabetes Tipo 2</li>
+                  <li>Diabetes Gestacional</li>
+                </ul>
+              </div>
+            </div>
           </div>
-          {/* Agrega más secciones de información aquí */}
+        </div>
+
+        <div className="row">
+          <div className="col-md-12">
+            <div className="card">
+              <div className="card-body">
+                <h2 className="card-title">Síntomas comunes:</h2>
+                <ul className="card-text">
+                  <li>Aumento de la sed</li>
+                  <li>Aumento de la micción</li>
+                  <li>Aumento del hambre</li>
+                  <li>Fatiga</li>
+                  <li>Pérdida de peso inexplicada</li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div className="row">
+          <div className="col-md-12">
+            <div className="card">
+              <div className="card-body">
+                <h2 className="card-title">Estrategias de manejo:</h2>
+                <ul className="card-text">
+                  <li>Control de la dieta</li>
+                  <li>Actividad física</li>
+                  <li>Monitoreo de glucosa</li>
+                  <li>Tratamiento farmacológico</li>
+                  <li>Educación en diabetes</li>
+                  <li>Manejo del estrés</li>
+                </ul>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </div>
